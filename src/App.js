9a53e7c7eb5d3e81443d3ecd3695c36489d4154e
losrobbos <losrobbos@web.de>
2021-06-04 11:12:30 +0200

    Polishing

--- src/App.js
+++ src/App.js
@@ -22,20 +22,6 @@ function App() {
     </div>
   );
 
-      // <Map center={position} zoom={zoom}>
-      //   <TileLayer
-      //     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      //     url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-      //   />
-      //   <Marker position={position}>
-      //     <Popup>
-      //       <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
-      //     </Popup>
-      //   </Marker>
-      // </Map>
-
-  // return (
-        
 }
 
 export default App;
